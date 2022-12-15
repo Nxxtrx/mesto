5887ed0464bff9df650659718c2ea6baf26c3ef2
Denis Valitov <ssilverufa@yandex.ru>
2022-12-15 14:31:38 +0500

    refactor: организовать файловую структуру по БЭМ

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -6,24 +6,28 @@ let popupOpened = page.querySelector('.popup_opened');
 let profileName = page.querySelector('.profile__name');
 let profileDescription = page.querySelector('.profile__subtitle');
 let submitButton = page.querySelector('.popup__btn-submit');
+let inputName = page.querySelector('.popup__profile-name');
+let inputDescription = page.querySelector('.popup__profile-description');
 
-
-
-function closePopup(e) {
+function closePopup() {
   if (popup.classList.contains('popup_opened')) {
     popup.classList.remove('popup_opened');
   } else {
     popup.classList.add('popup_opened');
   }
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+};
 
-  let inputName = page.querySelector('.popup__profile-name');
-  let inputDescription = page.querySelector('.popup__profile-description');
-
+function submitPopup(e) {
   profileName.textContent = `${inputName.value}`;
   profileDescription.textContent = `${inputDescription.value}`;
+  closePopup();
   e.preventDefault();
 };
 
-submitButton.addEventListener('click', closePopup);
+
+
+submitButton.addEventListener('click', submitPopup);
 closeButtom.addEventListener('click', closePopup);
 editProfile.addEventListener('click', closePopup);
