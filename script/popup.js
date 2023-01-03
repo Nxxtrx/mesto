6368a880355994125e64e48f2f4a67f0bae7f8ef
Denis Valitov <ssilverufa@yandex.ru>
2023-01-03 23:44:53 +0500

    feat:сделать добавление карточек на стрицу через массив

--- script/popup.js
+++ script/popup.js
@@ -19,6 +19,8 @@ function closePopup() {
   inputDescription.value = profileDescription.textContent;
 };
 
+const editPopup = page.querySelector('.popup__edit');
+
 closeButtom.addEventListener('click', closePopup);
 editProfile.addEventListener('click', closePopup);
 
@@ -32,3 +34,50 @@ function submitPopup(e) {
 
 formPopup.addEventListener('submit', submitPopup);
 
+
+
+// Добавление массива со стандартными карточками
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Добавление карточек на страницу из массива
+
+const cardsTemplate = page.querySelector('#cards-template').content;
+const cardList = page.querySelector('.cards__list');
+
+initialCards.forEach( (item) => {
+  const cardsElement = cardsTemplate.querySelector('.cards__item').cloneNode(true);
+  cardsElement.querySelector('.cards__subtitle').textContent = item.name;
+  cardsElement.querySelector('.cards__image').src = item.link;
+  cardList.append(cardsElement);
+});
+
+// popup окно добавления карточки
+
+const addMestoPopup = page.querySelector('.popup__add-item');
+const addCards = page.querySelector('.profile__add-button');
