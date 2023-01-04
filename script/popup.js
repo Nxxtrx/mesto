fe908646b0bcf95b6aa7cca723ef1ae9ef1dbdf3
Denis Valitov <ssilverufa@yandex.ru>
2023-01-04 22:13:41 +0500

    feat: Добавить открытие и закрытие popup окна через одну функцию

--- script/popup.js
+++ script/popup.js
@@ -1,6 +1,6 @@
 let page = document.querySelector('.page');
-let closeButtom = page.querySelector('.popup__close');
-let popup = page.querySelector('.popup');
+let closeButtom = page.querySelectorAll('.popup__close');
+let popup = page.querySelectorAll('.popup');
 let editProfile = page.querySelector('.profile__edit-button');
 let popupOpened = page.querySelector('.popup_opened');
 let profileName = page.querySelector('.profile__name');
@@ -9,31 +9,38 @@ let inputName = page.querySelector('.popup__profile-edit_type_name');
 let inputDescription = page.querySelector('.popup__profile-edit_type_description');
 let formPopup = page.querySelector('.popup__form')
 
-function closePopup() {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened');
-  } else {
-    popup.classList.add('popup_opened');
-  }
+// Функция открытия и закрытия попап окон
+
+function closePopup({modal}) {
+    popup.forEach( (item) => {
+      if (item.classList === modal.classList || item.classList.contains('popap_opened'))  {
+        item.classList.add('popup_opened');
+      } else {
+        item.classList.remove('popup_opened');
+      }
+    });
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 };
 
+// добавление кнопок для открытия и закрытия попап окна для редактирования профиля
+
+closeButtom.forEach ((elem) => {
+  elem.addEventListener('click', () => {closePopup({modal: popup})});
+});
 const editPopup = page.querySelector('.popup__edit');
+editProfile.addEventListener('click', () => {closePopup({modal: editPopup})});
 
-closeButtom.addEventListener('click', closePopup);
-editProfile.addEventListener('click', closePopup);
-
+// функция для сохранения введеных данных для редактирования профиля
 
 function submitPopup(e) {
   e.preventDefault();
   profileName.textContent = `${inputName.value}`;
   profileDescription.textContent = `${inputDescription.value}`;
-  closePopup();
+  closePopup({modal: popup})
 };
 
 formPopup.addEventListener('submit', submitPopup);
-
 
 
 // Добавление массива со стандартными карточками
@@ -81,3 +88,4 @@ initialCards.forEach( (item) => {
 
 const addMestoPopup = page.querySelector('.popup__add-item');
 const addCards = page.querySelector('.profile__add-button');
+addCards.addEventListener('click', () => {closePopup({modal: addMestoPopup})});
